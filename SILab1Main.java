c98b0cb1424e734089ce59159d8ad5f3f898a345
Stefan Andonov <stefan5andonov@yahoo.com>
2024-03-19 12:57:23 +0100

    Fix error

--- SILab1Main.java
+++ SILab1Main.java
@@ -10,7 +10,7 @@ public class SILab1Main {
 
         List<String> list = new ArrayList<>();
 
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             list.add(sc.next());
         }
     }
